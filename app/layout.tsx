eb9728b29dc6a3c4ac6977fdbbe863f50cbe291e
aps-dna6 <zananouri5@gmail.com>
2024-08-29 22:13:10 +0330

    add panel dashbord

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import Providers from '@/components/layout/providers';
+import { Toaster } from '@/components/ui/toaster';
+import '@uploadthing/react/styles.css';
+import type { Metadata } from 'next';
+import NextTopLoader from 'nextjs-toploader';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Next Shadcn',
+  description: 'Basic dashboard with Next.js and Shadcn'
+};
+
+export default async function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`overflow-hidden font-serif`}
+        suppressHydrationWarning={true}
+      >
+        <NextTopLoader showSpinner={false} />
+        <Providers>
+          <Toaster />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
